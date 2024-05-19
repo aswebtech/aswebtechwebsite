88dbdc7b0dfa74f6ec42505bfa4d58ed5f2a3084
aswebtech <149572220+aswebtech@users.noreply.github.com>
2024-05-19 19:34:35 +0530

    Update GraphicDesign.js

--- src/view/Services/GraphicDesign.js
+++ src/view/Services/GraphicDesign.js
@@ -15,7 +15,7 @@ function GraphicDesign() {
                     <img src={process.env.PUBLIC_URL + "/Assets/services/graphicdesign.jpg"} className="App-banner" alt="logo" />
                     <div style={{ position: "relative" }}>
                         <div className="head_wrapper">
-                            <h1><span id="head_main">GraphicDesign DEVELOPMENT</span></h1>
+                            <h1><span id="head_main">Graphic Design</span></h1>
                             <p><span id="head_para">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</span></p>
                             <button className="allBtnwrapper">GET IN TOUCH</button>
                         </div>
