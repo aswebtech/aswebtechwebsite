0c2abe7c36c4a7373433504da705ae13cbfee3be
aswebtech <149572220+aswebtech@users.noreply.github.com>
2024-05-19 14:44:49 +0530

    Update TopHeaderInfo.js

--- src/view/home/topHeadInfo/TopHeaderInfo.js
+++ src/view/home/topHeadInfo/TopHeaderInfo.js
@@ -8,8 +8,6 @@ function TopHeaderInfo() {
                     <Container>
                     <Row>
                         <ul>
-                            <li><a href="">Projects</a></li>
-                            <li><a href="">Book a Meeting</a></li>
                             <li><a href="">+8630081871</a></li>
                             <li><a href="">WhatsApp Now</a></li>
                         </ul>
